d47b480ddb6cdf59e1cfdff77fff21d5b2cff7ee
TarasevichVlad24 <gagagdet@gmail.com>
2023-02-02 15:40:08 +0300

    add const regex

--- src/console/ConsoleApplication.java
+++ src/console/ConsoleApplication.java
@@ -7,16 +7,17 @@ import service.UserService;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.regex.Pattern;
 
 import static console.ConsoleReader.*;
-
-import static console.ConsoleWriter.*;
+import static console.ConsoleWriter.write;
 
 public class ConsoleApplication {
     public static ConsoleSession consoleSession;
     private final UserService userService = new UserService();
     private final CalculatorService calculatorService = new CalculatorService();
+    private final String NAME_REGEX = "^[A-Z]{1}[a-z]{1,12}$";
+    private final String PASSWORD_REGEX = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,}$";
+
 
     public void run() {
         while (true) {
@@ -24,56 +25,57 @@ public class ConsoleApplication {
                 write("Hello Guest");
                 write("1 - Registration, 2 - Autorization, 3 - Exit");
                 int i = readInt();
-                while(true) {
-                    switch (i) {
-                        case 1:
-                            write("Enter name");
-                            String name = readString();
-                            Pattern p = Pattern.compile("^[A-Z]{1}[a-z]{1,12}$");
-                            if (!name.matches(p.pattern())) {
-                                ConsoleWriter.write(name + " is invalid name");
-                                continue;
-                            }
-                            write("Enter username");
-                            String username = readString();
-                            write("Enter password");
-                            String pass = readString();
-                            Pattern pp = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,}$");
-                            if (!pass.matches(p.pattern())) {
-                                ConsoleWriter.write(pass + " is invalid password");
-                                continue;
-                            }
-                            User user = new User(username, pass, name);
-                            userService.create(user);
-                            continue;
-                        case 2:
-                            write("Enter username");
-                            String username1 = readString();
-                            write("Enter password");
-                            String pass1 = readString();
-                            Optional<User> byUserName = userService.findByUserName(username1);
-                            if (byUserName.isPresent()) {
-                                User user1 = byUserName.get();
-                                if (user1.getPassword().equals(pass1)) {
-                                    consoleSession = new ConsoleSession(user1);
-                                    continue;
+                switch (i) {
+                    case 1:
 
-                                } else {
-                                    write("Wrong password");
-                                    continue;
-                                }
+                        write("Enter name");
+                        String name = readString();
+                        if (!name.matches(NAME_REGEX)) {
+                            ConsoleWriter.writeErr(name + " is invalid name");
+                            continue;
+                        }
+                        write("Enter username");
+                        String username = readString();
+                        write("Enter password");
+                        String pass = readString();
+                        if (!pass.matches(PASSWORD_REGEX)) {
+                            ConsoleWriter.writeErr(pass + " is invalid password");
+
+
+                        }
+                        User user = new User(username, pass, name);
+                        userService.create(user);
+                        continue;
+
+
+                    case 2:
+                        write("Enter username");
+                        String username1 = readString();
+                        write("Enter password");
+                        String pass1 = readString();
+                        Optional<User> byUserName = userService.findByUserName(username1);
+                        if (byUserName.isPresent()) {
+                            User user1 = byUserName.get();
+                            if (user1.getPassword().equals(pass1)) {
+                                consoleSession = new ConsoleSession(user1);
+                                continue;
+
                             } else {
-                                write("User not found!");
+                                write("Wrong password");
                                 continue;
                             }
-                        case 3:
-                            return;
-                    }
+                        } else {
+                            write("User not found!");
+                            continue;
+                        }
+                    case 3:
+                        return;
                 }
+
             } else {
                 write("Hello " + consoleSession.getCurrentUser().getName());
                 write("1 - Calculator, 2 - History, 3 - Logout, 4 - Exit");
-                switch (readInt()){
+                switch (readInt()) {
                     case 1:
                         write("Enter num1");
                         double num1 = readDouble();
@@ -92,19 +94,19 @@ public class ConsoleApplication {
 
                     case 2:
                         write("1 - CheckAllHistory, 2 - RemoveAllHistory,3 - FindById");
-                        switch(readInt()) {
+                        switch (readInt()) {
                             case 1:
                                 List<Operation> allByUser = calculatorService.findAllByUser(consoleSession.getCurrentUser());
-                            for (Operation operation1 : allByUser) {
-                                write(operation1.toString());
-                            }
-                            continue;
+                                for (Operation operation1 : allByUser) {
+                                    write(operation1.toString());
+                                }
+                                continue;
                             case 2:
                                 calculatorService.removeAll(consoleSession.getCurrentUser());
                                 continue;
                             case 3:
                                 write("enter id");
-                                calculatorService.findByIdOperation( readInt());
+                                calculatorService.findByIdOperation(readInt());
                                 continue;
                         }
                         continue;
--- src/console/ConsoleWriter.java
+++ src/console/ConsoleWriter.java
@@ -4,4 +4,7 @@ public final class ConsoleWriter {
     public static void write(String message){
         System.out.println(message);
     }
+    public static void writeErr(String message){
+        System.err.println(message);
+    }
 }
